e77c1e393f05a81ce7c7e6e08a3c02b346bec6f0
vaishnav90 <vaishnavanand90@gmail.com>
2025-12-23 16:33:33 -0800

    Add empty autonomous OpMode files for different starting positions and alliances

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/anime/opmodes/AnimeAutoCenter.java
@@ -0,0 +1,21 @@
+package org.firstinspires.ftc.teamcode.anime.opmodes;
+
+import com.bylazar.configurables.annotations.Configurable;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+@Configurable
+@Autonomous(name="Anime: Auto Center", group="Anime")
+public class AnimeAutoCenter extends LinearOpMode {
+
+    @Override
+    public void runOpMode() {
+        // Initialize hardware here
+        
+        waitForStart();
+        
+        // Autonomous code here (30 seconds max)
+        
+    }
+}
+
